e74d9260637e63bd73d09ac29703b6a181f666d8
Rafael Cardeal Lodi <65565453+rafaelcardeallodi@users.noreply.github.com>
2022-01-09 19:20:34 -0300

    Trocando de página

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,18 +1,17 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useState } from "react";
 import { RiAddLine } from "react-icons/ri";
-import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { api } from "../../services/api";
 import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList(){
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [ page, setPage ] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -71,7 +70,7 @@ export default function UserList(){
                     </Tr>
                   </Thead>
                   <Tbody>
-                    {data.map(user => (
+                    {data.users.map(user => (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
                           <Checkbox colorScheme="pink" />
@@ -89,9 +88,9 @@ export default function UserList(){
                 </Table>
                 
                 <Pagination 
-                  totalCountOfRegisters={200}
-                  currentPage={5}
-                  onPageChange={() => {}}
+                  totalCountOfRegisters={data.totalCount}
+                  currentPage={page}
+                  onPageChange={setPage}
                 />
               </>
             )}
